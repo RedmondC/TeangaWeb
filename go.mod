3d23d7733863b0dd500bd17a244e06072e9b4e81
Colin Redmond <colinredmond@proton.me>
2024-12-08 11:28:59 +0000

    feat: added new assets and templates

--- go.mod
+++ go.mod
@@ -27,13 +27,21 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.13.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.12.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/detectors/gcp v1.29.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
--- main.go
+++ main.go
@@ -1,118 +1,39 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"html/template"
-	"log"
+	"errors"
+	"log/slog"
 	"net/http"
+
 	"teangaWeb/templates"
 
-	firebase "firebase.google.com/go"
-	"google.golang.org/api/option"
-)
-
-type User struct {
-	Username     string
-	Subscription string
-}
-
-var user = User{Username: "john_doe", Subscription: "Free"}
-
-const (
-	firebaseConfigFile = "path/to/your/firebaseConfig.json"
-	firebaseDBURL      = "https://your-firebase-project.firebaseio.com"
-)
-
-var (
-	ctx context.Context
-	app *firebase.App
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// Initialize Firebase
-	ctx = context.Background()
-	opt := option.WithCredentialsFile(firebaseConfigFile)
-	app, err := firebase.NewApp(ctx, nil, opt)
-	if err != nil {
-		log.Fatalf("Firebase initialization error: %v\n", err)
-	}
+	e := echo.New()
 
-	// Initialize Firestore
-	temp, err := app.DatabaseWithURL(ctx, firebaseDBURL)
-	if err != nil {
-		log.Fatalf("Firestore initialization error: %v\n", err)
-	}
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	temp = temp
-	http.HandleFunc("/", serveTemplate)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/login-container", loginContainer)
-	http.HandleFunc("/get-username", getUsername)
-	http.HandleFunc("/get-editable-username", getEditableUserName)
-	http.HandleFunc("/update-username", updateUsername)
-	http.HandleFunc("/get-subscription", getSubscription)
-	http.HandleFunc("/toggle-subscription", toggleSubscription)
+	e.Static("/assets", "assets")
+	e.GET("/", basicLayout)
+	//e.GET("/*", RouteNotFoundHandler) 404 page
 
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-
-	log.Println("Server started on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(w, nil)
-}
-
-func loginContainer(w http.ResponseWriter, r *http.Request) {
-	templates.Login(false, "").Render(context.Background(), w)
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	if r.FormValue("email") == "colin@colin.com" && r.FormValue("password") == "password" {
-		templates.UserPage().Render(context.Background(), w)
-	} else {
-		templates.Login(true, "Incorrect credentials").Render(context.Background(), w)
+	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start server", "error", err)
 	}
 }
 
-// Get current username
-func getUsername(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(user.Username))
+func basicLayout(c echo.Context) error {
+	return renderView(c, templates.BasicInfoLayout())
 }
 
-func getEditableUserName(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`<span id="username-display"><form hx-post="/update-username" hx-target="#username-display" hx-swap="outerHTML">
-	<input type="text" name="username" placeholder="Enter new username" required>
-	<button type="submit">Save</button>
-  </form></span>`))
-}
+func renderView(c echo.Context, cmp templ.Component) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
 
-// Update username
-func updateUsername(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	user.Username = r.FormValue("username")
-
-	w.Write([]byte(`<span id="username-display">` + user.Username + `</span>`)) // move to templ
-}
-
-// Get current subscription
-func getSubscription(w http.ResponseWriter, r *http.Request) {
-	response, _ := json.Marshal(map[string]string{"subscription": user.Subscription})
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`<span id="subscription-display">` + user.Subscription + `</span>`))
-	print(response)
-}
-
-// Toggle subscription
-func toggleSubscription(w http.ResponseWriter, r *http.Request) {
-	if user.Subscription == "Free" {
-		user.Subscription = "Paid"
-	} else {
-		user.Subscription = "Free"
-	}
-	w.Write([]byte(`<span id="subscription-display">` + user.Subscription + `</span>`))
+	return cmp.Render(c.Request().Context(), c.Response().Writer)
 }
