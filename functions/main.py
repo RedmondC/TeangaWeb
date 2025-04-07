1aaf90496d061333a02113eca699b0eeec93a517
Colin Redmond <colinredmond@proton.me>
2025-04-07 18:55:40 +0100

    feat: fixed badge layout and added gunicorn

--- functions/main.py
+++ functions/main.py
@@ -1,7 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+app.config['DEBUG'] = False
 
-@app.route("/hello")
-def hello():
-    return jsonify({"message": "Hello from Python API!"})
+@app.route("/")
+def main():
+    logo = url_for("static", filename="header_with_dara_knot.png")
+    layered_screens = url_for("static", filename="layered_screens.png")
+    google_badge = url_for("static", filename="GetItOnGooglePlay_Badge_Web_color_English.png")
+    apple_badge = url_for("static", filename="Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.png")
+    return render_template("index.html", logo=logo, layered_screens=layered_screens, google_badge=google_badge, apple_badge=apple_badge)
