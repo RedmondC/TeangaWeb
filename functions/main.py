91f3938d5829d05243d61f8db627e0014ddc1bd1
Colin Redmond <colinredmond@proton.me>
2025-06-01 09:11:43 +0100

    added dev apple endpoint

--- functions/main.py
+++ functions/main.py
@@ -73,6 +73,23 @@ def verify_apple():
         logging.error("Apple - An unexpected error occurred for notification: ", data)
         return "An unexpected error occurred.", 500
 
+@app.route("dev/sandbox-user-update-apple", methods=["POST"])
+def verify_apple():
+    data = request.get_json()
+    signed_payload = data.get("signedPayload")
+    try:
+        if not signed_payload:
+            logging.info("No JSON signed payload received in request %s", data)
+            return "Invalid JSON", 400
+        else:
+            apple_notifications(signed_payload, subscriptions_reference)
+    except VerificationException:
+        logging.exception("Failed to handle Apple notification: ", data)
+        return "An error occurred", 400
+    except Exception:
+        logging.error("Apple - An unexpected error occurred for notification: ", data)
+        return "An unexpected error occurred.", 500
+
 
 @app.route("/")
 def main():
