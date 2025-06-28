4970f1a28c7d46d70e259a140b5fde4c9dbb363e
Colin Redmond <colinredmond@proton.me>
2025-06-28 12:10:34 +0100

    added delete me page

--- functions/main.py
+++ functions/main.py
@@ -90,6 +90,23 @@ def verify_apple_dev():
         logging.error("Apple - An unexpected error occurred for notification: ", data)
         return "An unexpected error occurred.", 500
 
+@app.route("/dev/sandbox-user-update-apple", methods=["GET"])
+def verify_apple_dev():
+    data = request.get_json()
+    signed_payload = data.get("signedPayload")
+    try:
+        if not signed_payload:
+            logging.info("No JSON signed payload received in request %s", data)
+            return "Invalid JSON", 400
+        else:
+            apple_notifications(signed_payload, subscriptions_reference)
+    except VerificationException:
+        logging.exception("Failed to handle Apple notification: ", data)
+        return "An error occurred", 400
+    except Exception:
+        logging.error("Apple - An unexpected error occurred for notification: ", data)
+        return "An unexpected error occurred.", 500
+
 
 @app.route("/privacy", methods=["GET"])
 def privacy():
